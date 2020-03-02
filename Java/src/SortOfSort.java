d13df5eb9ac011572b9aa0b3193f8838e2abda64
Krystian Carrillo <krycar12@gmail.com>
2020-03-02 14:47:28 -0700

    Started SortOfSort method

--- Java/src/SortOfSort.java
+++ Java/src/SortOfSort.java
@@ -1,5 +1,5 @@
 public class SortOfSort{
-    public void sortOfSort(int[] arry) {
+    public void sortOfSort(int[] arry){
         int maxNum = arry[0];
 
         for (int i = 0; i < arry.length; i++) {
@@ -7,5 +7,9 @@ public class SortOfSort{
                 maxNum = arry[i];
         }
         System.out.println(maxNum);
+
+        //LMAO this is probably the wrong way to start but ill figure it out later *heart eye emoji*
+        //Im baby UwU
+        //my mental stability decreasing at a rapid rate and it cant be stopped *Sun Glasses emoji*
     }
 }
