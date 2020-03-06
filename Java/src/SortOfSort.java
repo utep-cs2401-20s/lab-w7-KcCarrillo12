eb3a4de3ed6c6fc696db757b21509273880a5fa4
Krystian Carrillo <krycar12@gmail.com>
2020-03-06 10:24:48 -0700

    Finished SortOfSort and SortOfSortTester

--- Java/src/SortOfSort.java
+++ Java/src/SortOfSort.java
@@ -1,29 +1,50 @@
 public class SortOfSort{
-    public static void main(String[] args) {
-        int[] arry = {7, 9, 3, 8, 20, 13, 1, 7};
+    public static void sortOfSort(int[] arry ){
+        int maxNum = 0;
+        int arryEnd = arry.length - 1;
+        int arryBeg = 0;
+        int counter = 0;
+        int numCurr = 0;
 
-        sortOfSort(arry);
-    }
+        while(arryBeg < arryEnd){
+            numCurr = arryBeg;
 
-    public static void sortOfSort(int [] arry){
-        int x=0;
-        int maxNum = arry[0];
-        int tempNum;
-        while(x < arry.length){
-            for (int i = 0; i < arry.length; i++) {
-                if(arry[i] > maxNum){
-                    maxNum = arry[i];
-                    tempNum = arry[arry.length-1];
-                    arry[arry.length-1] = maxNum;
-                    arry[i] = tempNum;
+            for(int i=arryBeg; i<=arryEnd; i++){
+                if(arry[i] > numCurr){
+                    numCurr = arry[i];
+                    maxNum = i;
                 }
             }
-            System.out.println(maxNum);
-            x++;
-        }
 
-        for(int i=0; i<arry.length; i++)
-            System.out.print(arry[i] + " ");
+            if(counter == 0){
+                swapArry(arry, maxNum, arryEnd);
+                counter++;
+                arryEnd--;
+            }
+            else if(counter == 1){
+                swapArry(arry, maxNum, arryEnd);
+                counter++;
+                arryEnd--;
+            }
+            else if(counter == 2){
+                swapArry(arry, maxNum, arryBeg);
+                counter++;
+                arryBeg++;
+            }
+            else{
+                swapArry(arry, maxNum, arryBeg);
+                counter++;
+                arryBeg++;
+            }
+            if(counter == 4){
+                counter = 0;
+            }
+        }
     }
 
+    public static void swapArry(int[] arry, int maxNum, int swap){ //helper method
+        int tempNum = arry[maxNum];
+        arry[maxNum] = arry[swap];
+        arry[swap] = tempNum;
+    }
 }
--- Java/src/SortOfSortTester.java
+++ Java/src/SortOfSortTester.java
@@ -2,14 +2,49 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class SortOfSortTester {
-    //5 test cases
-    //put comments for the test cases.
 
-/*    @Test //defaut test case (i dont even think its written correctly lol)
-    public void sortOfSort1() {
+  @Test
+    public void sortOfSort1(){ //This test case tests the method sortOfSort (Passed)
         int[] arry = {7, 9, 3, 8, 20, 13, 1, 7};
         int[] testArr = {9, 8, 3, 1, 7, 7, 13, 20};
         SortOfSort sol = new SortOfSort();
-        assertArrayEquals(testArr, sol.sortOfSort(arry)); */
-    //}
+        sol.sortOfSort(arry);
+        assertArrayEquals(testArr, arry);
+    }
+
+    @Test
+    public void sortOfSort2(){ //This test case tests if sortOfSort method can handel odd arrays (Passed)
+        int[] arry = {10, 3, 9, 8, 5};
+        int[] testArr = {8, 5, 3, 9, 10};
+        SortOfSort sol = new SortOfSort();
+        sol.sortOfSort(arry);
+        assertArrayEquals(testArr, arry);
+    }
+
+    @Test
+    public void sortOfSort3(){ //This test case tests if sortOfSort method can handel an
+        int[] arry = {10, 5, 5, 10, 2, 2, 9, 9}; //array of multiple duplicate numbers (Passed)
+        int[] testArr = {9, 9, 2, 2, 5, 5, 10, 10};
+        SortOfSort sol = new SortOfSort();
+        sol.sortOfSort(arry);
+        assertArrayEquals(testArr, arry);
+    }
+
+    @Test
+    public void sortOfSort4(){ //This test case tests if sortOfSort method can handel large arrays (Passed)
+        int[] arry = {16, 3, 15, 9, 13, 1, 19, 17, 10, 8};
+        int[] testArr = {16, 15, 9, 8, 1, 3, 10, 13, 17, 19};
+        SortOfSort sol = new SortOfSort();
+        sol.sortOfSort(arry);
+        assertArrayEquals(testArr, arry);
+    }
+
+    @Test
+    public void sortOfSort5(){ //This test case tests if sortOfSort method can handel short arrays
+        int[] arry = {0, -2};  //with negative numbers (Passed)
+        int[] testArr ={-2, 0};
+        SortOfSort sol = new SortOfSort();
+        sol.sortOfSort(arry);
+        assertArrayEquals(testArr, arry);
+    }
 }
